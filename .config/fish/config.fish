024676c55c770aca1d8bae4a21e66d87f7e33ba2
nikersify <nikersifyyt@gmail.com>
2016-12-04 15:04:53 +0100

    bang bang

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,7 @@ set -g theme_display_date no
 set -g theme_display_cmd_duration no
 set -g theme_show_exit_status no
 set -g theme_color_scheme base16-light
+
+function sudo!!
+	eval sudo $history[1]
+end
