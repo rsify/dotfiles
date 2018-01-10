5ce22033c8acb503dc2c9e6ed59b41d8309f38f9
nikersify <nikersifyyt@gmail.com>
2018-01-10 12:26:32 +0100

    alias stuff

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,4 +1,39 @@
+# brew
+# ====
+
+alias b "brew"
+alias bcd "brew cask doctor"
+alias bch "brew cask help"
+alias bci "brew cask info"
+alias bcin "brew cask install"
+alias bcl "brew cask list"
+alias bcri "brew cask reinstall"
+alias bcs "brew cask search"
+alias bcu "brew cask upgrade"
+alias bcun "brew cask uninstall"
+alias bd "brew doctor"
+alias bh "brew help"
+alias bi "brew info"
+alias bin "brew install"
+alias bl "brew list"
+alias bri "brew reinstall"
+alias bs "brew search"
+alias bt "brew tap"
+alias bug "brew upgrade"
+alias bun "brew uninstall"
+alias bup "brew update"
+alias but "brew untap"
+
+# config
+# ======
+
+alias ca "vim ~/.config/fish/alias.fish"
+alias cf "vim ~/.config/fish/config.fish"
+alias cv "vim ~/.vim/vimrc"
+
 # git
+# ===
+
 alias ga "git add"
 alias gap "git add -p"
 
@@ -29,42 +64,87 @@ alias gr "git reset HEAD"
 alias gs "git status"
 
 function git --description "Alias for hub, which wraps git to provide extra functionality with GitHub."
-    hub $argv
+	hub $argv
 end
 
 # tmux
+# ====
+
 alias ta "tmux attach -t"
 alias tk "tmux kill-session -t"
 alias tls "tmux ls"
 
 function tn --wraps tmux --description "tmux new"
 	if test -n "$argv"
-		tmux new -s "$argv"
+		# start session in ~/dev/<argv> if the dir exists
+		if test -d ~/dev/$argv
+			tmux new -s "$argv" -c ~/dev/$argv
+		else
+			tmux new -s "$argv"
+		end
 	else
 		tmux new -s (sort -R .config/misc/words | head -n 1)
 	end
 end
 
 # mac
+# ===
+
 function ding --description "Show a notification"
-    set st $status
+	set st $status
 
-    set desc "$_ - $PWD"
+	set desc "$_ - $PWD"
 
-    set title "Ding!"
-    if test -n "$argv"
-	set title "$argv"
-    end
+	set title "Ding!"
+	if test -n "$argv"
+		set title "$argv"
+	end
 
-    set sound "hero"
-    # play scary sound if last cmd errored
-    if test ! $st -eq 0
-	set sound "sosumi"
-	set err "Error ($st) - "
-    end
+	set sound "hero"
+	# play scary sound if last cmd errored
+	if test ! $st -eq 0
+		set sound "sosumi"
+		set err "Error ($st) - "
+	end
 
-    switch (uname)
-	case Darwin
-	    /usr/bin/osascript -e "display notification \"$desc\" with title \"$err$title\" sound name \"$sound\""
-    end
+	switch (uname)
+		case Darwin
+			/usr/bin/osascript -e "display notification \"$desc\" with title \"$err$title\" sound name \"$sound\""
+	end
+end
+
+function _prefix_url_with_http
+	set res $argv
+
+	if test -n "$argv"
+		if not string match 'http*://*' $res
+			set res "http://"$res
+		end
+	end
+
+	echo $res
+end
+
+function safari --description 'Open Safari.app'
+	open -a 'safari' (_prefix_url_with_http $argv)
+end
+
+function firefox --description 'Open Firefox.app'
+	open -a 'firefox' (_prefix_url_with_http $argv)
+end
+
+function chrome --description 'Open Google Chrome.app'
+	open -a 'google chrome' (_prefix_url_with_http $argv)
+end
+
+# local files
+alias chromel 'open -a "google chrome"'
+alias firefoxl 'open -a firefox'
+alias safaril 'open -a safari'
+
+# misc
+# ====
+
+function sudo!!
+	eval sudo $history[1]
 end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,10 +6,6 @@ set -g theme_display_cmd_duration no
 set -g theme_show_exit_status no
 set -g theme_color_scheme base16-light
 
-function sudo!!
-	eval sudo $history[1]
-end
-
 function _path
 	set -gx PATH $PATH $argv
 end
