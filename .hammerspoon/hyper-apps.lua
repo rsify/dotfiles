3237cd646b2b806a61ed79fa996559a30fa7d0ee
nikersify <nikersifyyt@gmail.com>
2018-04-15 20:26:20 +0200

    replace slate with hammerspoon

---
+++ .hammerspoon/hyper-apps.lua
@@ -0,0 +1,17 @@
+return {
+	{ '3', 'MacDown' },
+	{ 'a', 'Slack' },
+	{ 'b', 'Airtable' },
+	{ 'c', 'Caprine' },
+	{ 'd', '2Do' },
+	{ 'f', 'FileZilla' },
+	{ 'g', 'Google Chrome' },
+	{ 'h', 'Insomnia' },
+	{ 'i', 'Discord' },
+	{ 'l', 'SnippetsLab' },
+	{ 'm', 'Mail' },
+	{ 'p', 'Postico' },
+	{ 'r', 'Microsoft Remote Desktop' },
+	{ 's', 'Safari' },
+	{ 't', 'kitty' }
+}
---
+++ .hammerspoon/init.lua
@@ -0,0 +1,19 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+	hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+end)
+
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
+	doReload = false
+	for _,file in pairs(files) do
+		if file:sub(-4) == ".lua" then
+			doReload = true
+		end
+	end
+	if doReload then
+		hs.reload()
+	end
+end):start()
+
+require('hyper')
+
+hs.notify.new({title='Hammerspoon', informativeText='Ready!'}):send()
