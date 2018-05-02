aacdf72a094fbe76e600abc9a8a569bab401ec6f
nikersify <nikersifyyt@gmail.com>
2018-05-02 09:54:16 +0200

    add hyper binds help message to hammerspoon

--- .hammerspoon/hyper.lua
+++ .hammerspoon/hyper.lua
@@ -1,7 +1,9 @@
 mappings = require('hyper-apps')
 
+hyper = {'shift', 'ctrl', 'alt', 'cmd'}
+
 for i, mapping in ipairs(mappings) do
-	hs.hotkey.bind({'shift', 'ctrl', 'alt', 'cmd'}, mapping[1], function()
+	hs.hotkey.bind(hyper, mapping[1], function()
 		local appName = mapping[2]
 		local app = hs.application.get(appName)
 		if app then
@@ -11,3 +13,27 @@ for i, mapping in ipairs(mappings) do
 		end
 	end)
 end
+
+help_alert_uuid = nil
+function show_help()
+	str = ''
+	for i, mapping in ipairs(mappings) do
+		str = str .. mapping[1] .. ' ⇒ ' .. mapping[2] .. '\n'
+	end
+
+	help_alert_uuid = hs.alert(str:sub(1, -2), {
+		atScreenEdge = 1,
+		textFont = 'DejaVu Sans Mono'
+	}, 'inf')
+end
+
+function hide_help()
+	print('hiding')
+	if help_alert_uuid then
+		print('hiding fo real')
+		hs.alert.closeSpecific(help_alert_uuid)
+		help_alert_uuid = nil
+	end
+end
+
+hs.hotkey.bind(hyper, '\\', show_help, hide_help)
