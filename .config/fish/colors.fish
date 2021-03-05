14f79260aa914b8996c11053402cb4554a474115
nikersify <nikersify@nikerino.com>
2021-03-05 11:09:19 +0100

    -dracula +nord

--- .config/fish/colors.fish
+++ .config/fish/colors.fish
@@ -1,25 +1,26 @@
-set -g fish_color_autosuggestion BD93F9
-set -g fish_color_cancel -r
-set -g fish_color_command white
-set -g fish_color_comment 6272A4
+# Nord from `fish_config` (tweaked)
+set -g fish_color_normal normal
+set -g fish_color_command 81a1c1 --bold
+set -g fish_color_quote a3be8c
+set -g fish_color_redirection b48ead
+set -g fish_color_end 88c0d0
+set -g fish_color_error ebcb8b
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_history_current --bold
+set -g fish_color_operator 00a6b2
+set -g fish_color_escape 00a6b2
 set -g fish_color_cwd green
 set -g fish_color_cwd_root red
-set -g fish_color_end 50FA7B
-set -g fish_color_error FFB86C
-set -g fish_color_escape bryellow --bold
-set -g fish_color_history_current --bold
-set -g fish_color_host normal
-set -g fish_color_match --background=brblue
-set -g fish_color_normal normal
-set -g fish_color_operator bryellow
-set -g fish_color_param FF79C6
-set -g fish_color_quote F1FA8C
-set -g fish_color_redirection 8BE9FD
-set -g fish_color_search_match bryellow --background=brblack
-set -g fish_color_selection white --bold --background=brblack
-set -g fish_color_user brgreen
 set -g fish_color_valid_path --underline
-set -g fish_pager_color_completion
+set -g fish_color_autosuggestion 4c566a
+set -g fish_color_user brgreen
+set -g fish_color_host normal
+set -g fish_color_cancel -r
+set -g fish_pager_color_completion normal
 set -g fish_pager_color_description B3A06D yellow
 set -g fish_pager_color_prefix white --bold --underline
 set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_color_match --background=brblue
+set -g fish_color_comment 434c5e
+set -g fish_color_param eceff4
