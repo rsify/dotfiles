3c0e2ba90858a96225aa05d461b1680c71b95ed1
Maciej Łaszcz <nikersify@nikerino.com>
2022-07-03 17:53:34 +0200

    vim: linebreak in markdown

--- .config/bin/alacritty-vaultpopup.fish
+++ .config/bin/alacritty-vaultpopup.fish
@@ -5,5 +5,5 @@ cd vet
 set -l out (ag --hidden --silent --ignore Archive --ignore .obsidian -g . | fzf --reverse --bind=alt-enter:print-query)
 
 if test $status = 0
-	vim -c ':set linebreak' $out
+	vim $out
 end
