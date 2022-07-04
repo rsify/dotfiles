3bb58afde7da2073bcc92fb2418d6aa64a5d9301
Maciej Łaszcz <nikersify@nikerino.com>
2022-07-04 14:24:14 +0200

    vim: underline highlight unused

--- .config/nvim/lua/rsify/colors.lua
+++ .config/nvim/lua/rsify/colors.lua
@@ -3,6 +3,9 @@ vim.cmd('colorscheme dracula')
 vim.g.dracula_full_special_attrs_support = 1
 vim.cmd('hi CursorLine guibg=#303241')
 
+-- temp ugly workaround for underline not working with coc.nvim + dracula
+vim.cmd('hi link CocUnusedHighlight DiagnosticUnderlineInfo')
+
 vim.api.nvim_create_autocmd("BufWinEnter", { -- highlight extra whitespace
 	pattern = "*",
 	callback = function ()
