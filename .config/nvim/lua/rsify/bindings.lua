8edfad0406a4e14f085c5acf079ee5ce9d0326b5
Maciej Łaszcz <nikersify@nikerino.com>
2022-07-30 07:04:52 +0200

    vim: coc bindings

--- .config/nvim/lua/rsify/bindings.lua
+++ .config/nvim/lua/rsify/bindings.lua
@@ -15,9 +15,10 @@ function show_documentation()
 	end
 end
 
+vim.cmd("command! -nargs=* -range CocFix    :call CocActionAsync('codeActionRange', <line1>, <line2>, 'quickfix')")
+
 vim.g.mapleader = ','
 
-vim.api.nvim_set_keymap('n', '<leader>c', ':CocList --normal --auto-preview diagnostics<CR>', { silent = true })
 vim.api.nvim_set_keymap('i', '<c-j>',     'copilot#Accept("<CR>")',             { expr = true })
 vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()',                      { silent = true, expr = true })
 vim.api.nvim_set_keymap('i', '<cr>',      '<c-g>u<cr><c-r>=coc#on_enter()<cr>', { silent = true, noremap = true })
@@ -32,10 +33,9 @@ vim.api.nvim_set_keymap('n', '<c-w>_',    '<c-w>s',                             
 vim.api.nvim_set_keymap('n', '<c-w>|',    '<c-w>v',                             { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<esc>',     ':nohlsearch<return><esc>',           { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>F', ':Telescope git_files<CR>',           { silent = true })
-vim.api.nvim_set_keymap('n', 'g[', ':CocPrev<CR>',                       { silent = true })
-vim.api.nvim_set_keymap('n', 'g]', ':CocNext<CR>',                       { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>a', '<Plug>(coc-codeaction)',             { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>b', '<Plug>(coc-references)',             { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>c', ':CocList --normal --auto-preview diagnostics<CR>', { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>d', '<Plug>(coc-definition)',             { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>e', ':CocListResume<CR>',                 { silent = true })
 vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope find_files<CR>',          { silent = true, noremap = true })
@@ -50,5 +50,7 @@ vim.api.nvim_set_keymap('n', '<space>',   ':Telescope buffers<CR>',             
 vim.api.nvim_set_keymap('n', 'K',         '',                                   { callback = show_documentation })
 vim.api.nvim_set_keymap('n', '[g',        '<Plug>(coc-diagnostic-prev)',        { silent = true })
 vim.api.nvim_set_keymap('n', ']g',        '<Plug>(coc-diagnostic-next)',        { silent = true })
+vim.api.nvim_set_keymap('n', 'g[',        ':CocPrev<CR>',                       { silent = true })
+vim.api.nvim_set_keymap('n', 'g]',        ':CocNext<CR>',                       { silent = true })
 
 vim.g.switch_mapping = '\\' -- switch.vim
